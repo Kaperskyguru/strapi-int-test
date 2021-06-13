c8702e23949b00d011af860019f62026872859b4
Solomon Eseme <kaperskyguru@gmail.com>
2021-06-14 00:08:29 +0100

    updated database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 let environment = process.env.NODE_ENV || "development";
 
+console.log(environment, "TEST TEST TEST");
+
 const config = require("../../knexfile.js")[environment];
 
 module.exports = require("knex")(config);
