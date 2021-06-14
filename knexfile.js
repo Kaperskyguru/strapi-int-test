cc37ae1d0d65daa05be29a51ef39de8314420d20
Solomon Eseme <kaperskyguru@gmail.com>
2021-06-14 01:25:43 +0100

    updated database config

--- knexfile.js
+++ knexfile.js
@@ -50,7 +50,9 @@ module.exports = {
       user: process.env.DB_USER || "postgres",
       password: process.env.DB_PASSWORD || "",
       host: process.env.DB_HOST || "127.0.0.1",
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
 
     pool: {
