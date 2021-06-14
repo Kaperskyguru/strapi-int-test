c4138e1932c20d4654a2bed3c6677aa31e35efaf
Solomon Eseme <kaperskyguru@gmail.com>
2021-06-14 01:15:47 +0100

    updated database config

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,8 @@ let environment = process.env.NODE_ENV || "development";
 
 const config = require("../../knexfile.js")[environment];
 
-if (config.production.client !== "sqlite3")
-  if (environment && environment === "production") {
-    const pg = require("pg");
-    pg.defaults.ssl = true;
-  }
+if (environment && environment === "production") {
+  const pg = require("pg");
+  pg.defaults.ssl = true;
+}
 module.exports = require("knex")(config);
